d57803bcc1184378488f0549b3b92697ef250f80
jremy813 <jremywatkins@gmail.com>
2020-09-15 15:09:11 -0400

    created content component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
-      {/* Content */}
+      <Content />
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Content.scss";
+
+function Content() {
+  return (
+    <>
+      <div className="content">
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default Content;
