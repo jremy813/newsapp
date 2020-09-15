54fd827548720cbc3553260b0fcfdd2fb60dffb0
jremy813 <jremywatkins@gmail.com>
2020-09-15 16:16:39 -0400

    created and styled search component and started the newsfeed component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./Content.scss";
 import Search from "./Search";
+import NewsFeed from "./NewsFeed";
 
 function Content() {
   return (
     <>
       <div className="content">
         <Search />
+        <NewsFeed />
       </div>
     </>
   );
