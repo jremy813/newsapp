0f8221cc86aa93643014c58eaf8e13379c8212aa
jremy813 <jremywatkins@gmail.com>
2020-09-16 15:51:25 -0400

    made fetch request for news data

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NewsFeed.scss";
 
 function NewsFeed() {
-  return <div></div>;
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=03712056389d4d30a0846278a2030a50&category=technology"
+    )
+      .then((res) => res.json())
+      .then((data) => setNews(data.articles));
+  }, []);
+  console.log(news);
+  return (
+    <div className="newsFeed">
+      {news.map((info) => (
+        <h1>{info.title}</h1>
+      ))}
+    </div>
+  );
 }
 
 export default NewsFeed;
