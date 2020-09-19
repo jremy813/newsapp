92b5088f42bb8787023b5e36cf3ca571548c9dd6
jremy813 <jremywatkins@gmail.com>
2020-09-19 18:18:40 -0400

    made headlines variable that displays data from search and categories

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -2,33 +2,29 @@ import React, { useEffect, useState } from "react";
 import "./NewsFeed.scss";
 
 function NewsFeed({ searchData, categoryData }) {
-  console.log(categoryData);
+  const [headlines, setHeadlines] = useState([]);
+
+  useEffect(() => {
+    if (searchData) {
+      setHeadlines(searchData);
+    }
+
+    if (categoryData) {
+      setHeadlines(categoryData);
+    }
+  }, []);
+
   return (
     <>
       <div className="newsFeed">
-        {!categoryData && !searchData ? <h1>No data</h1> : null}
-        {
-          (categoryData
-            ? categoryData.map((info) => (
-                <div className="newsFeed__Card">
-                  <h3>{info.title}</h3>
-                  <span>{info.source.name}</span>
-                  <span>{info.author}</span>
-                  <p>{info.description}</p>
-                </div>
-              ))
-            : null,
-          searchData
-            ? searchData.map((info) => (
-                <div className="newsFeed__Card">
-                  <h3>{info.title}</h3>
-                  <span>{info.source.name}</span>
-                  <span>{info.author}</span>
-                  <p>{info.description}</p>
-                </div>
-              ))
-            : null)
-        }
+        {headlines.map((info) => (
+          <div className="newsFeed__Card">
+            <h3>{info.title}</h3>
+            <span>{info.source.name}</span>
+            <span>{info.author}</span>
+            <p>{info.description}</p>
+          </div>
+        ))}
       </div>
     </>
   );
