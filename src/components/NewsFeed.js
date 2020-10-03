2e55dcd6d3858acaee26215410d193d3fe6af7e6
jremy813 <jremywatkins@gmail.com>
2020-10-03 15:24:11 -0400

    removed headlines data

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -2,19 +2,15 @@ import React, { useEffect, useState } from "react";
 import "./NewsFeed.scss";
 
 function NewsFeed({ searchData, categoryData }) {
-  const [headlines, setHeadlines] = useState([]);
-
   return (
     <>
       <div className="newsFeed">
-        {headlines.map((info) => (
-          <div className="newsFeed__Card">
-            <h3>{info.title}</h3>
-            <span>{info.source.name}</span>
-            <span>{info.author}</span>
-            <p>{info.description}</p>
-          </div>
-        ))}
+        <div className="newsFeed__Card">
+          <h3>{info.title}</h3>
+          <span>{info.source.name}</span>
+          <span>{info.author}</span>
+          <p>{info.description}</p>
+        </div>
       </div>
     </>
   );
